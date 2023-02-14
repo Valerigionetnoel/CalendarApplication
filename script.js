4c874aca8f0e6beda18d0a03175e3c8525c6635d
Valéri Gionet-Noël <valeri.gionetnoel@gmail.com>
2023-02-14 15:57:56 -0400

    Complete the project. I don't see any error any more.

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
+// My variable for most of the project.
 var currentDay = dayjs()
 var past = document.getElementsByClassName('past')
 var present = document.getElementsByClassName('present')
 var future = document.getElementsByClassName('future')
-var hours = [9, 10, 11, 12, 13, 14, 15, 16, 17]
 
-
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// Function to start once the page is done loading. 
 $(document).ready(function () {
+
+  // Select the save button, and add a function to it that save in local storage.
   $('.saveBtn').on('click', function(){
     var timeBlock = $(this).parent().attr('id');
     var textValue = $(this).siblings('.description').val()
     localStorage.setItem(timeBlock, textValue)
   })
   
+  var currentHour = dayjs().hour()
+
+  // Logic for class in the time sheet.
+  for (var i = 9; i < 18; i++){
+    var timeBlockHour = $("#hour-"+i)
+    if (currentHour > i){
+      timeBlockHour.addClass('past')
+    } else if (currentHour === i) {
+      timeBlockHour.addClass('present')
+    }else {
+      timeBlockHour.addClass('future')
+    }
+  }
+  
+  // This is where we get the local storage item and put it in its right div.
   $('#hour-9 .description').val(localStorage.getItem('hour-9'))
   $('#hour-10 .description').val(localStorage.getItem('hour-10'))
   $('#hour-11 .description').val(localStorage.getItem('hour-11'))
@@ -25,44 +39,7 @@ $(document).ready(function () {
   $('#hour-16 .description').val(localStorage.getItem('hour-16'))
   $('#hour-17 .description').val(localStorage.getItem('hour-17'))
 
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // $('hour-9').css('background-color', 'blue')
-  for (var i = 0; i < hours.length; i++){
-    var timeBlockHour = hours[i]
-   console.log(timeBlockHour)
-    if (currentDay.hour() > timeBlockHour){
-      console.log('past')
-      $(this).addClass('past')
-    } else if (currentDay.hour() === timeBlockHour) {
-      $(this).addClass('present')
-      console.log('present')
-    }else {
-      $(this).addClass('future')
-      console.log('future')
-    }
-  }
-  
-
-  `hour-${timeBlockHour}`
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-
-$('#currentDay').text(currentDay.format('MMM D, YYYY HH:mm'))
+  //This put the date and time at the top of the page.
+  $('#currentDay').text(currentDay.format('ddd MMM D, YYYY HH:mm'))
 });
 
